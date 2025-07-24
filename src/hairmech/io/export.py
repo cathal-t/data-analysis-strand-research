8a44685520f102c455e60dbba11c06ebc6a14f3a
cathal-t <133865580+cathal-t@users.noreply.github.com>
2025-07-24 19:20:38 -0400

    Update export.py

--- src/hairmech/io/export.py
+++ src/hairmech/io/export.py
@@ -100,5 +100,5 @@ def save_stats_wide(
                     {"type": "formula",
                      "criteria": f'=AND(ISNUMBER({let}{first}),{let}{first}<0.05)',
                      "format": f_sig})
-print("[INFO] Metrics →", ROOT / f"{ROOT.name}_metrics.xlsx")
-print("[INFO] Stats   →", ROOT / f"{ROOT.name}_stats.xlsx")
+print("[INFO] Metrics →", ROOT / f"test_metrics.xlsx")
+print("[INFO] Stats   →", ROOT / f"test_stats.xlsx")
